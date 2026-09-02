df37fdfc71318d7c9caedd59d69e0756ce8809d1
Bryce Porter <sliceco@gmail.com>
2026-09-02 02:27:56 -0600

    Fix particle flags

--- src/particle.h
+++ src/particle.h
@@ -2,16 +2,15 @@
 
 namespace godot {
 
-struct Particle {
-    std::uint8_t mat_id = 0;
-    std::uint8_t flags = ParticleFlags::PARTICLE_FLAG_NONE;
-};
-
-
 enum ParticleFlags : std::uint8_t {
     PARTICLE_FLAG_NONE    = 0,
     PARTICLE_FLAG_UPDATED = (1 << 0),
     PARTICLE_FLAG_BURNING = (1 << 1) // Easy to expand later
+};
+
+struct Particle {
+    std::uint8_t mat_id = 0;
+    std::uint8_t flags = ParticleFlags::PARTICLE_FLAG_NONE;
 };
 
 } // namespace godot
